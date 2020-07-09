75cc916b3910856bdb651b42e8182bdeafec9003
Pavel Nedosika <nedosika@gmail.com>
2020-07-09 22:24:00 +0300

    add react material dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {ThemeProvider} from '@material-ui/styles';
+import {createMuiTheme} from '@material-ui/core';
 
-function App() {
-    return (
-        <div>
-            Hello
-        </div>
-    );
+import {
+    BrowserRouter as Router,
+} from "react-router-dom";
+
+import Routes from './Routes';
+
+const theme = createMuiTheme({
+        zIndex:
+            {
+                appBar: 1200,
+                drawer: 1100
+            }
+    }
+)
+
+export default class App extends Component {
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <Router>
+                    <Routes/>
+                </Router>
+            </ThemeProvider>
+        );
+    }
 }
-
-export default App;
