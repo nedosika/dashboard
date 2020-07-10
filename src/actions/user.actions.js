ace27adc620db49eef5ba2dc4a53c27dda236176
nedosika <nedosika@gmail.com>
2020-07-10 12:59:25 +0300

    add actions

---
+++ src/actions/user.actions.js
@@ -0,0 +1,34 @@
+import { userConstants } from '../constants';
+import { userService } from '../services';
+
+const login = (username, password) => {
+    return dispatch => {
+        dispatch(request({ username }));
+
+        userService.login(username, password)
+            .then(
+                user => {
+                    dispatch(success(user));
+                    history.push('/');
+                },
+                error => {
+                    dispatch(failure(error));
+                    //dispatch(alertActions.error(error));
+                }
+            );
+    };
+
+    const request = (user) => { return { type: userConstants.LOGIN_REQUEST, user } }
+    const success = (user) => { return { type: userConstants.LOGIN_SUCCESS, user } }
+    const failure = (error) => { return { type: userConstants.LOGIN_FAILURE, error } }
+}
+
+const logout = () => {
+    userService.logout();
+    return { type: userConstants.LOGOUT };
+}
+
+export const userActions = {
+    login,
+    logout,
+};
---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import users from './users.reducer';
+import authentication from "./authentication.reducer";
+
+
+const rootReducer = combineReducers({
+    users,
+    authentication
+});
+
+export default rootReducer;
